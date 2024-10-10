253e62ae61ed646a620f0fc962d65c3d6aad2151
Stefano <valenzano.stefano99@gmail.com>
2024-10-10 15:27:57 +0200

    Update paths for pdf.js dependencies

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: normalizePath(path.resolve(__dirname, './node_modules/pdf.js/l10n')),
+          src: normalizePath(path.resolve(__dirname, '../../node_modules/pdf.js/l10n')),
           dest: ''
         }
       ]
@@ -37,38 +37,38 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'pdfjs': fileURLToPath(new URL('./node_modules/pdf.js/src', import.meta.url)),
-      'pdfjs-lib': fileURLToPath(new URL('./node_modules/pdf.js/src/pdf.js', import.meta.url)),
-      'pdfjs-web': fileURLToPath(new URL('./node_modules/pdf.js/web', import.meta.url)),
+      'pdfjs': fileURLToPath(new URL('../../node_modules/pdf.js/src', import.meta.url)),
+      'pdfjs-lib': fileURLToPath(new URL('../../node_modules/pdf.js/src/pdf.js', import.meta.url)),
+      'pdfjs-web': fileURLToPath(new URL('../../node_modules/pdf.js/web', import.meta.url)),
 
       'fluent-bundle': fileURLToPath(new URL('../../node_modules/@fluent/bundle/esm/index.js', import.meta.url)),
       'fluent-dom': fileURLToPath(new URL('../../node_modules/@fluent/dom/esm/index.js', import.meta.url)),
       'cached-iterable': fileURLToPath(new URL('../../node_modules/cached-iterable/src/index.mjs', import.meta.url)),
 
-      'display-fetch_stream': fileURLToPath(new URL('./node_modules/pdf.js/src/display/fetch_stream.js', import.meta.url)),
-      'display-network': fileURLToPath(new URL('./node_modules/pdf.js/src/display/network.js', import.meta.url)),
-      'display-node_stream': fileURLToPath(new URL('./node_modules/pdf.js/src/display/stubs.js', import.meta.url)),
-      'display-node_utils': fileURLToPath(new URL('./node_modules/pdf.js/src/display/stubs.js', import.meta.url)),
+      'display-fetch_stream': fileURLToPath(new URL('../../node_modules/pdf.js/src/display/fetch_stream.js', import.meta.url)),
+      'display-network': fileURLToPath(new URL('../../node_modules/pdf.js/src/display/network.js', import.meta.url)),
+      'display-node_stream': fileURLToPath(new URL('../../node_modules/pdf.js/src/display/stubs.js', import.meta.url)),
+      'display-node_utils': fileURLToPath(new URL('../../node_modules/pdf.js/src/display/stubs.js', import.meta.url)),
 
-      'web-alt_text_manager': fileURLToPath(new URL('./node_modules/pdf.js/web/alt_text_manager.js', import.meta.url)),
-      'web-annotation_editor_params': fileURLToPath(new URL('./node_modules/pdf.js/web/annotation_editor_params.js', import.meta.url)),
-      'web-download_manager': fileURLToPath(new URL('./node_modules/pdf.js/web/download_manager.js', import.meta.url)),
-      'web-external_services': fileURLToPath(new URL('./node_modules/pdf.js/web/genericcom.js', import.meta.url)),
-      'web-new_alt_text_manager': fileURLToPath(new URL('./node_modules/pdf.js/web/new_alt_text_manager.js', import.meta.url)),
-      'web-null_l10n': fileURLToPath(new URL('./node_modules/pdf.js/web/genericl10n.js', import.meta.url)),
-      'web-pdf_attachment_viewer': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_attachment_viewer.js', import.meta.url)),
-      'web-pdf_cursor_tools': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_cursor_tools.js', import.meta.url)),
-      'web-pdf_document_properties': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_document_properties.js', import.meta.url)),
-      'web-pdf_find_bar': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_find_bar.js', import.meta.url)),
-      'web-pdf_layer_viewer': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_layer_viewer.js', import.meta.url)),
-      'web-pdf_outline_viewer': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_outline_viewer.js', import.meta.url)),
-      'web-pdf_presentation_mode': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_presentation_mode.js', import.meta.url)),
-      'web-pdf_sidebar': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_sidebar.js', import.meta.url)),
-      'web-pdf_thumbnail_viewer': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_thumbnail_viewer.js', import.meta.url)),
-      'web-preferences': fileURLToPath(new URL('./node_modules/pdf.js/web/genericcom.js', import.meta.url)),
-      'web-print_service': fileURLToPath(new URL('./node_modules/pdf.js/web/pdf_print_service.js', import.meta.url)),
-      'web-secondary_toolbar': fileURLToPath(new URL('./node_modules/pdf.js/web/secondary_toolbar.js', import.meta.url)),
-      'web-toolbar': fileURLToPath(new URL('./node_modules/pdf.js/web/toolbar.js', import.meta.url)),
+      'web-alt_text_manager': fileURLToPath(new URL('../../node_modules/pdf.js/web/alt_text_manager.js', import.meta.url)),
+      'web-annotation_editor_params': fileURLToPath(new URL('../../node_modules/pdf.js/web/annotation_editor_params.js', import.meta.url)),
+      'web-download_manager': fileURLToPath(new URL('../../node_modules/pdf.js/web/download_manager.js', import.meta.url)),
+      'web-external_services': fileURLToPath(new URL('../../node_modules/pdf.js/web/genericcom.js', import.meta.url)),
+      'web-new_alt_text_manager': fileURLToPath(new URL('../../node_modules/pdf.js/web/new_alt_text_manager.js', import.meta.url)),
+      'web-null_l10n': fileURLToPath(new URL('../../node_modules/pdf.js/web/genericl10n.js', import.meta.url)),
+      'web-pdf_attachment_viewer': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_attachment_viewer.js', import.meta.url)),
+      'web-pdf_cursor_tools': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_cursor_tools.js', import.meta.url)),
+      'web-pdf_document_properties': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_document_properties.js', import.meta.url)),
+      'web-pdf_find_bar': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_find_bar.js', import.meta.url)),
+      'web-pdf_layer_viewer': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_layer_viewer.js', import.meta.url)),
+      'web-pdf_outline_viewer': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_outline_viewer.js', import.meta.url)),
+      'web-pdf_presentation_mode': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_presentation_mode.js', import.meta.url)),
+      'web-pdf_sidebar': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_sidebar.js', import.meta.url)),
+      'web-pdf_thumbnail_viewer': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_thumbnail_viewer.js', import.meta.url)),
+      'web-preferences': fileURLToPath(new URL('../../node_modules/pdf.js/web/genericcom.js', import.meta.url)),
+      'web-print_service': fileURLToPath(new URL('../../node_modules/pdf.js/web/pdf_print_service.js', import.meta.url)),
+      'web-secondary_toolbar': fileURLToPath(new URL('../../node_modules/pdf.js/web/secondary_toolbar.js', import.meta.url)),
+      'web-toolbar': fileURLToPath(new URL('../../node_modules/pdf.js/web/toolbar.js', import.meta.url)),
     }
   },
 })
